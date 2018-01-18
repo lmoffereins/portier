46fa04481dad55134c98ae45ac007ad9cd8c5050
Laurens Offereins <lmoffereins@gmail.com>
2018-01-18 09:08:00 +0100

    Move admin logic to seperate file

--- portier.php
+++ portier.php
@@ -113,6 +113,7 @@ final class Portier {
 
 		// Admin
 		if ( is_admin() ) {
+			require( $this->includes_dir . 'admin.php'    );
 			require( $this->includes_dir . 'settings.php' );
 		}
 	}
@@ -133,14 +134,10 @@ final class Portier {
 		add_filter( 'login_message',     array( $this, 'login_message'  ), 1 );
 		add_action( 'admin_bar_menu',    array( $this, 'admin_bar_menu' )    );
 
-
 		// Admin
-		add_action( 'admin_init',         array( $this, 'register_settings'     ) );
-		add_action( 'admin_menu',         array( $this, 'admin_menu'            ) );
-		add_action( 'portier_admin_head', array( $this, 'enqueue_admin_scripts' ) );
-
-		// Plugin links
-		add_filter( 'plugin_action_links', array( $this, 'settings_link' ), 10, 2 );
+		if ( is_admin() ) {
+			add_action( 'init', 'portier_admin' );
+		}
 
 		// Setup extensions
 		add_action( 'bp_loaded', 'portier_setup_buddypress' );
@@ -339,160 +336,6 @@ final class Portier {
 		</style>
 
 		<?php
-	}
-
-	/** Admin *****************************************************************/
-
-	/**
-	 * Create the plugin admin page menu item
-	 *
-	 * @since 1.0.0
-	 */
-	public function admin_menu() {
-
-		// Setup settings page
-		$hook = add_options_page(
-			esc_html__( 'Portier Settings', 'portier' ),
-			esc_html__( 'Portier', 'portier' ),
-			'manage_options',
-			'portier',
-			array( $this, 'admin_page' )
-		);
-
-		add_action( "admin_head-$hook",   array( $this, 'admin_head'   ) );
-		add_action( "admin_footer-$hook", array( $this, 'admin_footer' ) );
-	}
-
-	/**
-	 * Enqueue script and style in plugin admin page head
-	 *
-	 * @since 1.0.0
-	 *
-	 * @uses do_action() Calls 'portier_admin_head'
-	 */
-	public function admin_head() {
-		do_action( 'portier_admin_head' );
-	}
-
-	/**
-	 * Output plugin admin page footer contents
-	 *
-	 * @since 1.0.0
-	 *
-	 * @uses do_action() Calls 'portier_admin_footer'
-	 */
-	public function admin_footer() { 
-		do_action( 'portier_admin_footer' );
-	}
-
-	/**
-	 * Output plugin admin page contents
-	 *
-	 * @since 1.0.0
-	 */
-	public function admin_page() { ?>
-
-		<div class="wrap">
-			<h1 class="wp-heading-inline"><?php esc_html_e( 'Portier', 'portier' ); ?></h1>
-
-			<form method="post" action="options.php">
-				<?php settings_fields( 'portier' ); ?>
-				<?php do_settings_sections( 'portier' ); ?>
-				<?php submit_button(); ?>
-			</form>
-		</div>
-
-		<?php
-	}
-
-	/**
-	 * Output admin page scripts and styles
-	 * 
-	 * @since 1.0.0
-	 */
-	public function enqueue_admin_scripts() {
-
-		// Register Chosen when not done already
-		if ( ! wp_script_is( 'chosen', 'registered' ) ) {
-			wp_register_script( 'chosen', $this->includes_url . 'assets/js/chosen/chosen.jquery.min.js', array( 'jquery' ), '1.2.0' );
-		}
-		wp_enqueue_script( 'chosen' );
-
-		if ( ! wp_style_is( 'chosen', 'registered' ) ) {
-			wp_register_style( 'chosen', $this->includes_url . 'assets/js/chosen/chosen.min.css', false, '1.2.0' );
-		}
-		wp_enqueue_style( 'chosen' );
-
-		// WP pointer
-		wp_enqueue_script( 'wp-pointer' );
-		wp_enqueue_style ( 'wp-pointer' ); 
-
-		// Plugin admin
-		wp_register_script( 'portier-admin', $this->includes_url . 'assets/js/portier-admin.js', array( 'jquery', 'chosen', 'wp-pointer' ), $this->version );
-		wp_enqueue_script ( 'portier-admin' );
-		wp_localize_script( 'portier-admin', 'portierAdminL10n', array(
-			'pointerContent' => sprintf( '<h3>%s</h3><p>%s</p>',
-				esc_html__( 'Site Protection', 'portier' ),
-				esc_html__( 'The shield icon will show the current state of the protection of this site. When site protection is active, it is colored accordingly.', 'portier' )
-			),
-			'settings' => array(
-				'showPointer' => is_admin_bar_showing() && current_user_can( 'manage_options' ) && ! in_array( 'portier_protection', explode( ',', get_user_meta( get_current_user_id(), 'dismissed_wp_pointers', true ) ) ),
-			)
-		) ); ?>
-
-		<style type="text/css">
-			.chzn-container-multi .chzn-choices .search-field input {
-				height: 25px !important;
-			}
-
-			.form-table div + label,
-			.form-table textarea + label {
-				display: block;
-			}
-		</style>
-
-		<?php
-	}
-
-	/**
-	 * Setup the plugin settings
-	 *
-	 * @since 1.0.0
-	 */
-	public function register_settings() {
-
-		// Create settings sections
-		add_settings_section( 'portier-options-access', esc_html__( 'Access Settings', 'portier' ), 'portier_access_settings_info', 'portier' );
-
-		// Loop all settings to register
-		foreach ( portier_settings() as $setting => $args ) {
-
-			// Only render field when label and callback are present
-			if ( isset( $args['label'] ) && isset( $args['callback'] ) ) {
-				add_settings_field( $setting, $args['label'], $args['callback'], $args['page'], $args['section'] );
-			}
-
-			register_setting( $args['page'], $setting, $args['sanitize_callback'] );
-		}
-	}
-
-	/**
-	 * Add a settings link to the plugin actions on plugin.php
-	 *
-	 * @since 1.0.0
-	 *
-	 * @param array $links The current plugin action links
-	 * @param string $file The current plugin file
-	 * @return array $links All current plugin action links
-	 */
-	public function settings_link( $links, $file ) {
-
-		// Add settings link for our plugin
-		if ( $file == $this->basename ) {
-			$links['settings'] = '<a href="' . add_query_arg( 'page', 'portier', 'options-general.php' ) . '">' . esc_html__( 'Settings', 'portier' ) . '</a>';
-		}
-
-		return $links;
 	}
 }
 
